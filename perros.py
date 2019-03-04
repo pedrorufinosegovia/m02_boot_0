5626d7a1a6c033d9bef208613184d451f4adff6a
pedrorufino <pedro.rufino.segovia@gmail.com>
2019-03-04 09:59:46 +0100

    perros2

--- perros.py
+++ perros.py
@@ -12,10 +12,38 @@ class Perro():
             
     def __str__(self):
         return "Soy el perro {}".format(self.nombre)
+    
+class Perroasistencia():
+    def __init__(self, nombre, edad, peso, amo):
+        Perro.__init__(self, nombre, edad, peso)
+        self.amo = amo
+        self.__trabajando = False
+    
+    def __str__(self):
+        print("perro de asistencia de {}".format(self.amo))
+        
+    def pasear(self):
+            print("ayudo ami dueño {}" .format(self.amo))
+    
+    def ladrar(self):
+        if self.trabajando:
+            print("No puedo ladrar")
+        else:
+            Perro.ladrar(self)
+            
+    def trabajando(self, valor=None):
+        if valor == None:
+            return self.__trabajando
+        else:
+            self.__trabajando = valor
+
 salchicho = Perro("salchicho", 3, 15)
 lola = Perro("lola", 5, 3.5)
 salchicho.ladrar()
 lola.ladrar()
-print(salchicho)
+daga = Perroasistencia("daga", 3, 12, "pedro")
+daga.__str__()
+
+
 
 
